ff23bb7eaf85db6f3cd5743a1182b7611b460731
AhmedAhmed31 <34033396+AhmedAhmed31@users.noreply.github.com>
2020-04-24 17:29:01 +0200

    URL fixed

--- post/urls.py
+++ post/urls.py
@@ -1,18 +1,20 @@
 from django.conf.urls import url
 from . import views
-app_name ='post'
 
+app_name = 'post'
 
+urlpatterns = [
 
-
-
-
-urlpatterns=[
-
-    url(r'^$',views.all_post,name='all_post'),
+    url(r'^$', views.all_post, name='all_post'),
     url(r'^(?P<id>\d+)$', views.post, name='post'),
+
+    
+
     url(r'^login$', views.login, name='login'),
-    url(r'^(?P<id>\d+)/signup$', views.signup, name='signup'),
+    url(r'^signup$', views.signup, name='signup'),
+
+
+
     url(r'^create$', views.create_post, name='create_post'),
     url(r'^(?P<id>\d+)/edit$', views.edit_post, name='edit_post'),
 ]
