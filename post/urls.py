b5e6f29052c278fceaf2ab3705188e12875c7d19
AhmedAhmed31 <34033396+AhmedAhmed31@users.noreply.github.com>
2020-04-24 17:24:13 +0200

    Login/sign up page

--- post/urls.py
+++ post/urls.py
@@ -11,7 +11,8 @@ urlpatterns=[
 
     url(r'^$',views.all_post,name='all_post'),
     url(r'^(?P<id>\d+)$', views.post, name='post'),
-
+    url(r'^login$', views.login, name='login'),
+    url(r'^(?P<id>\d+)/signup$', views.signup, name='signup'),
     url(r'^create$', views.create_post, name='create_post'),
     url(r'^(?P<id>\d+)/edit$', views.edit_post, name='edit_post'),
 ]
--- post/views.py
+++ post/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from .forms import PostForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 # Create your views here.
@@ -13,17 +14,49 @@ def all_post(request):
 
 
 def post(request, id):
-    post = get_object_or_404(Post,id=id)
+    post = get_object_or_404(Post, id=id)
     context = {
         'post': post,
     }
     return render(request, 'single_post.html', context)
 
 
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    else:
+        form = UserCreationForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'signup.html', context)
+
+
+def login(request):
+
+    if request.method == 'POST':
+        form =AuthenticationForm()
+    else:
+
+        form = AuthenticationForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'login.html', context)
+
+
 def edit_post(request, id):
     post = get_object_or_404(Post, id=id)
     if request.method == 'POST':
-        form = PostForm(request.POST,request.FILES, instance=post)
+        form = PostForm(request.POST, request.FILES, instance=post)
         if form.is_valid():
             new_form = form.save(commit=False)
             new_form.user = request.user
@@ -42,7 +75,7 @@ def edit_post(request, id):
 
 def create_post(request):
     if request.method == 'POST':
-        form = PostForm(request.POST,request.FILES)
+        form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             new_form = form.save(commit=False)
             new_form.user = request.user
